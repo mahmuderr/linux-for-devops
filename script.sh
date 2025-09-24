2b23468778380dfb447bba98dcde74a3aa4f4340
Mahmuder Rahman <35294919+mahmuderr@users.noreply.github.com>
2025-09-24 10:02:54 +0600

    Create script.sh
    script.sh

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# this script will take unput from user to enter their email and password and then print the output
+
+read -p "enter your email :" email
+
+read -p "enter your password:" password
+
+echo "Your email is: $email"
+echo "Your password is: $password"
